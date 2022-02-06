019d3616e58bd119cd3e06ba79ac2128e2a3b494
mbret <bret.maxime@gmail.com>
2022-02-06 23:02:13 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const design = {
+  palette: {
+    foo: `bar`
+  },
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      type: `umd`
+    },
+    clean: true
+  },
+};
