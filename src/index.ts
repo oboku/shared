2038a7af1dcbcc4b420bc68644fc6e864ead3333
mbret <bret.maxime@gmail.com>
2022-02-06 23:41:45 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 export const design = {
   palette: {
-    foo: `bar`
+    orange: `rgb(225, 100, 50, 1)`
   },
+}
+
+export const links = {
+  documentation: `https://docs.oboku.me`,
+  app: `https://app.oboku.me`,
+  linkedin: `https://www.linkedin.com/in/maxime-bret`,
 }
